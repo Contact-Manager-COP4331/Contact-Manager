fbd07e7ba4fecc4f9af9bbdeaf56fe1605e84520
carterdyl <dylancar38@gmail.com>
2025-08-29 14:06:02 -0400

    Update register.php
    Register commit

--- LAMPAPI/register.php
+++ LAMPAPI/register.php
@@ -1,1 +1,39 @@
+<?php
+	$inData = getRequestInfo();
+    
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$login = $inData["login"];
+	$password = $inData["password"];
+
+	// Hash the password
+	$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) {
+		returnWithError($conn->connect_error);
+	} 
+	else {
+		$stmt = $conn->prepare("INSERT INTO Users (Login, Password, FirstName, LastName) VALUES (?, ?, ?, ?)");
+		$stmt->bind_param("ssss", $login, $hashedPassword, $firstName, $lastName);
+
+		if ($stmt->execute()) {
+			echo json_encode(["message" => "User added."]);
+		}
+		else {
+			echo json_encode(["message" => "Error: " . $stmt->error]);
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo() {
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function returnWithError($err) {
+		echo json_encode(["message" => $err]);
+	}
+?>
 
