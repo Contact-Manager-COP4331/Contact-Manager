136e31665b9d030784b424d8b0087711c0174080
carterdyl <dylancar38@gmail.com>
2025-09-04 13:21:13 -0400

    Update deleteContact.php
    Copied from AddContact but changed to delete instead of insert

--- LAMPAPI/deleteContact.php
+++ LAMPAPI/deleteContact.php
@@ -1,1 +1,43 @@
+<?php
+	$inData = getRequestInfo();
 
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP433119");
+	if ($conn->connect_error) 
+	{
+		returnWithError($conn->connect_error);
+	} 
+	else
+	{
+		$stmt = $conn->prepare("DELETE FROM Contacts Where (FirstName, LastName, UserID) VALUES (?,?,?)");
+		$stmt->bind_param("ssi", $inData["firstName"], $inData["lastName"], $inData["userId"]);
+
+		if ($stmt->execute())
+		{
+			returnWithError(""); // success = no error
+		}
+		else
+		{
+			returnWithError("Insert failed");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson($obj)
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError($err)
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+?>
