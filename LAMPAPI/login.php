b854440a52e69e5c2496d75956b68d705495de79
carterdyl <dylancar38@gmail.com>
2025-09-08 17:31:26 -0400

    Update login.php
    Copied from loginHash.php, which was an updated version of login.php that worked with hashed passwords. Only difference is implementing password_verify() function and accurate error/info output.

--- LAMPAPI/login.php
+++ LAMPAPI/login.php
@@ -16,14 +16,21 @@ if( $conn->connect_error )
 	}
 else
 	{
-		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
-		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
+		$stmt = $conn->prepare("SELECT ID,firstName,lastName, Password FROM Users WHERE Login=?");
+		$stmt->bind_param("s", $inData["login"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
 		if( $row = $result->fetch_assoc()  )
 		{
-			returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
+			if(password_verify($inData["password"], $row["Password"]))
+			{
+				returnWithInfo( $row["firstName"], $row["lastName"], $row["ID"] );
+			}
+			else
+			{
+				returnWithError("Incorrect Password");
+			}
 		}
 		else
 		{
