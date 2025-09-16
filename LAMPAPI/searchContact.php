0dc543af9efc1087d6597776254351357d427b8b
carterdyl <dylancar38@gmail.com>
2025-09-15 20:35:16 -0400

    Update searchContact.php
    Added comments

--- LAMPAPI/searchContact.php
+++ LAMPAPI/searchContact.php
@@ -1,4 +1,3 @@
-
 <?php
 
 	$inData = getRequestInfo();
@@ -6,6 +5,7 @@
 	$searchResults = "";
 	$searchCount = 0;
 
+	//Connects to database
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP433119");
 	if ($conn->connect_error) 
 	{
@@ -13,20 +13,24 @@
 	} 
 	else
 	{
+		//Binds parameters
 		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE (FirstName LIKE ? OR LastName LIKE ? OR Phone LIKE ? OR Email LIKE ? OR CreatedAt LIKE ? OR UpdatedAt LIKE ?) AND UserID=?");
 		$search = "%" . $inData["search"] . "%";
 		$stmt->bind_param("ssssssi", $search, $search, $search, $search, $search, $search, $inData["userId"]);
 		$stmt->execute();
 		
 		$result = $stmt->get_result();
-		
+
+		//Loops through all matching rows
 		while($row = $result->fetch_assoc())
 		{
+			//Adds comma between search results
 			if( $searchCount > 0 )
 			{
 				$searchResults .= ",";
 			}
 			$searchCount++;
+			//Appends full contact data to search results
 			$searchResults .= '{"firstName" : "' . $row["FirstName"]. '", 
                           "lastName" : "' . $row["LastName"]. '",
                           "phone" : "' . $row["Phone"]. '",
@@ -34,7 +38,8 @@
 						  "createdAt" : "' . $row["CreatedAt"]. '",
 						  "updatedAt" : "' . $row["UpdatedAt"]. '"}';
 		}
-		
+
+		//Returns results depending on if the search exists in the database
 		if( $searchCount == 0 )
 		{
 			returnWithError( "No Contact Found" );
@@ -58,13 +63,15 @@
 		header('Content-type: application/json');
 		echo $obj;
 	}
-	
+
+	//Returns error response
 	function returnWithError( $err )
 	{
 		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
+
+	//Returns successful contact from search
 	function returnWithInfo( $searchResults )
 	{
 		$retValue = '{"results":[' . $searchResults . '],"error":""}';
